36f6b30f790c68488db681222bcd6c2487e2897d
David <bigway2626@gmail.com>
2019-06-30 19:58:08 +0800

    init crud

--- web/web/server/index.js
+++ web/web/server/index.js
@@ -1,22 +1,66 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const db = require('./db/db');
+
 const app = new Koa();
+const router = new Router();
 
-const db = require('db');
-
-app.use(async ctx => {
-  const notes = [
-    { number: 123, name: 'David', content: 'sss' }
-  ];
-
-  db('notes')
-    .insert(notes)
-    .then(() => {
-      ctx.body = "data inserted";
-    }).catch((err) => {
-      ctx.body = err;
-    }).finally(() => {
-      db.destroy();
-    });
+router.get('/', (ctx, next) => {
+  ctx.body = 'ss';
 });
+
+async function getNotes() {
+  const notes = await db.select().table('notes');
+
+  return notes;
+}
+
+router.get('/list', async (ctx) => {
+  const notes = await db.select().table('notes');
+
+  ctx.body = notes;
+});
+
+router.get('/insert', async (ctx) => {
+  try {
+    const notes = [
+      { number: 52642, name: 'Audi', content: '56565' },
+    ];
+
+    await db('notes').insert(notes);
+
+    ctx.body = 'data inserted!';
+  } catch (error) {
+    ctx.body = error;
+  }
+});
+
+router.get('/delete', async (ctx) => {
+  try {
+    await db('notes')
+      .where('name', 'David')
+      .del();
+
+    ctx.body = 'data deleted!';
+  } catch (error) {
+    ctx.body = error;
+  }
+});
+
+router.get('/update', async (ctx) => {
+  try {
+    await db('notes')
+      .where('name', 'Audi')
+      .update({
+        name: 'Benz'
+      })
+
+    ctx.body = 'data updated!';
+  } catch (error) {
+    ctx.body = error;
+  }
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
