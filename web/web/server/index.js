6cf609995190c57f8376f58accdca6728c88bf1d
David <bigway2626@gmail.com>
2019-06-30 16:17:58 +0800

    test insert data

--- web/web/server/index.js
+++ web/web/server/index.js
@@ -1,8 +1,22 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const db = require('db');
+
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  const notes = [
+    { number: 123, name: 'David', content: 'sss' }
+  ];
+
+  db('notes')
+    .insert(notes)
+    .then(() => {
+      ctx.body = "data inserted";
+    }).catch((err) => {
+      ctx.body = err;
+    }).finally(() => {
+      db.destroy();
+    });
 });
 
 app.listen(3000);
